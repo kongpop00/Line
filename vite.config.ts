b34780c7458d72acaf20ce917af0656628b84c2b
K-ongpop Nualsaard <kongpopku@ku.th>
2023-12-13 10:34:20 +0700

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import express from './express-plugin' //Add this
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), express('/server')],  // Adjust this
+})
